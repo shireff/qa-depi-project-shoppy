0f016db847a11732e8ba8495990fe5c7e83fb915
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2025-04-06 12:47:20 +0200

    Updating AdminDashboardPage according to Static methods

--- src/main/java/com/shoppy/com/pages/AdminDashboardPage.java
+++ src/main/java/com/shoppy/com/pages/AdminDashboardPage.java
@@ -1,6 +1,7 @@
 package com.shoppy.com.pages;
 
 import DriverFactory.Driver;
+import com.shoppy.com.utils.ElementActions;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 
@@ -12,69 +13,68 @@ public class AdminDashboardPage {
     private final By productsBtn = By.xpath("//div[@class=\"flex text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground cursor-pointer\"][2]");
     private final By ordersdBtn = By.xpath("//div[@class=\"flex text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground cursor-pointer\"][3]");
     private final By logoutBtn = By.xpath("//div[@class=\"flex flex-1 justify-end\"]/button");
-
     //    /****************the rest is the footer and its buttons ************************/
     public AdminDashboardPage(Driver driver) {
         this.driver = driver;
     }
 
     public AdminDashboardPage checkAdminDashboardUrl() {
-        Assert.assertEquals(driver.browser().getCurrentURL(), URL);
+        Assert.assertEquals(driver.browser().getCurrentURL(driver.get()), URL);
         return this;
     }
 
     public AdminDashboardPage checkAdminPanelHeaderText() {
-        Assert.assertTrue(driver.element().getTextOf(adminPanelHeader).contains("Admin Panel"));
+        Assert.assertTrue(ElementActions.getText(driver.get(), adminPanelHeader).contains("Admin Panel"));
         return this;
     }
 
     public AdminDashboardPage checkAdminPanelHeaderDisplayed() {
-        Assert.assertTrue(driver.element().isDisplayed(adminPanelHeader));
+        Assert.assertTrue(ElementActions.isDisplayed(driver.get(), adminPanelHeader));
         return this;
     }
 
     public AdminDashboardPage checkDashboardBtnText() {
-        Assert.assertTrue(driver.element().getTextOf(dashboardBtn).contains("Dashboard"));
+        Assert.assertTrue(ElementActions.getText(driver.get(),dashboardBtn).contains("Dashboard"));
         return this;
     }
 
     public AdminDashboardPage checkDashboardBtnDisplayed() {
-        Assert.assertTrue(driver.element().isDisplayed(dashboardBtn));
+        Assert.assertTrue(ElementActions.isDisplayed(driver.get(),dashboardBtn));
         return this;
     }
 
     public AdminDashboardPage checkProductsBtnText() {
-        Assert.assertTrue(driver.element().getTextOf(productsBtn).contains("Products"));
+        Assert.assertTrue(ElementActions.getText(driver.get(), productsBtn).contains("Products"));
         return this;
     }
 
     public AdminDashboardPage checkProductsBtnDisplayed() {
-        Assert.assertTrue(driver.element().isDisplayed(productsBtn));
+        Assert.assertTrue(ElementActions.isDisplayed(driver.get(), productsBtn));
         return this;
     }
 
     public AdminDashboardPage checkOrdersdBtnText() {
-        Assert.assertTrue(driver.element().getTextOf(ordersdBtn).contains("Orders"));
+        Assert.assertTrue(ElementActions.getText(driver.get(), ordersdBtn).contains("Orders"));
         return this;
     }
 
     public AdminDashboardPage checkOrdersdBtnDisplayed() {
-        Assert.assertTrue(driver.element().isDisplayed(ordersdBtn));
+        Assert.assertTrue(ElementActions.isDisplayed(driver.get(), ordersdBtn));
         return this;
     }
 
     public AdminDashboardPage checkLogoutBtnText() {
-        Assert.assertTrue(driver.element().getTextOf(logoutBtn).contains(" Logout"));
+        Assert.assertTrue(ElementActions.getText(driver.get(), logoutBtn).contains(" Logout"));
         return this;
     }
 
     public AdminDashboardPage checkLogoutBtnDisplayed() {
-        Assert.assertTrue(driver.element().isDisplayed(logoutBtn));
+        Assert.assertTrue(ElementActions.isDisplayed(driver.get(), logoutBtn));
         return this;
     }
 
     public AdminProductsPage clickOnProductsBtn() {
-        driver.element().click(productsBtn);
+        ElementActions.click(driver.get(), productsBtn);
         return new AdminProductsPage(driver);
     }
 }
