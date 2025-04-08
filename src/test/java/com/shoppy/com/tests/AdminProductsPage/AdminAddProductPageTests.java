1fe93e4a876065de5a36f6c3c5f7c3a56d86ffc4
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2025-04-08 06:45:11 +0200

    Modifying AdminAddProductPageTests file

--- src/test/java/com/shoppy/com/tests/AdminProductsPage/AdminAddProductPageTests.java
+++ src/test/java/com/shoppy/com/tests/AdminProductsPage/AdminAddProductPageTests.java
@@ -1,41 +1,40 @@
 package com.shoppy.com.tests.AdminProductsPage;
 
 import DriverFactory.Driver;
+import com.shoppy.com.pages.AdminAddProductPage;
 import com.shoppy.com.pages.AdminDashboardPage;
+import com.shoppy.com.pages.AdminProductsPage;
 import com.shoppy.com.pages.LoginPage;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
 public class AdminAddProductPageTests {
     public Driver driver;
     protected LoginPage loginPage;
-    private String URL = "https://shoppy-ochre.vercel.app/auth/login";
+    private String LoginUrl = "https://shoppy-ochre.vercel.app/auth/login";
+    private String productsUrl = "https://shoppy-ochre.vercel.app/admin/products";
 
-    @BeforeMethod
+    @BeforeClass
     public void setup() {
         driver = new Driver("CHROME");
         driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.browser().openUrl(driver.get(), URL);
+        driver.browser().openUrl(driver.get(), LoginUrl);
         loginPage = new LoginPage(driver.get());
         loginPage.setUserName("Arima@example.com");
         loginPage.setPassword("123456789");
         loginPage.clickLogin();
-        new AdminDashboardPage(driver)
-                .clickOnProductsBtn()
-                .checkProductsPageUrl()
-                .checkAddProductBtnIsDisplayed()
-                .checkProductsAreDisplayed();
+
     }
 
     @Test
-    public void test() {
+    public void verifyThatAddProductButtonIsWorking() {
 
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown() throws InterruptedException {
         Thread.sleep(2000);
         driver.browser().closeBrowser(driver.get());
